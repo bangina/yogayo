5d0b2aa740d840f50e0f873dcbbf048796c60123
ChoiHaesol <enfzls1212@gmail.com>
2020-08-17 13:47:13 +0900

    table haeder 수정

--- src/style/tableStyle.js
+++ src/style/tableStyle.js
@@ -1,35 +1,8 @@
-import {
-    warningColor,
-    primaryColor,
-    dangerColor,
-    successColor,
-    infoColor,
-    roseColor,
-    grayColor,
-    defaultFont
-  } from "./material-dashboard-react.js";
-  
+
   const tableStyle = theme => ({
-    warningTableHeader: {
-      color: warningColor[0]
-    },
+    
     primaryTableHeader: {
-      color: primaryColor[0]
-    },
-    dangerTableHeader: {
-      color: dangerColor[0]
-    },
-    successTableHeader: {
-      color: successColor[0]
-    },
-    infoTableHeader: {
-      color: infoColor[0]
-    },
-    roseTableHeader: {
-      color: roseColor[0]
-    },
-    grayTableHeader: {
-      color: grayColor[0]
+      color: "#cf556c"
     },
     table: {
       marginBottom: "0",
@@ -41,13 +14,11 @@ import {
     },
     tableHeadCell: {
       color: "inherit",
-      ...defaultFont,
       "&, &$tableCell": {
         fontSize: "1em"
       }
     },
     tableCell: {
-      ...defaultFont,
       lineHeight: "1.42857143",
       padding: "12px 8px",
       verticalAlign: "middle",
