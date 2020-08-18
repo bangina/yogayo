0ff285cde09d5cb2ca7a23472cff9f8599ee0cae
Dory <enfzls1212@gmail.com>
2020-08-18 14:46:57 +0900

    해솔커밋 게시판 리덕스 추가 + 페이징 기능

---
+++ src/redux/posts.jsx
@@ -0,0 +1,61 @@
+const initialState = [
+  [1, "gravida", "egestas. Aliquam", "Lacota", "Oct 26, 2020"],
+  [
+    2,
+    "dis",
+    "mauris a nunc. In at pede. Cras vulputate velit eu",
+    "Kyla",
+    "Jul 17, 2020",
+  ],
+  [3, "ridiculus", "lectus.", "Medge", "Feb 22, 2021"],
+  [
+    4,
+    "elementum",
+    "libero est, congue a, aliquet vel,",
+    "Justina",
+    "Apr 1, 2020",
+  ],
+  [5, "dolor", "felis eget varius ultrices,", "Hermione", "Apr 20, 2021"],
+  [
+    6,
+    "Vestibulum",
+    "commodo tincidunt nibh. Phasellus nulla.",
+    "Meredith",
+    "Nov 6, 2019",
+  ],
+  [7, "diam.", "nec", "Darryl", "Nov 13, 2019"],
+  [8, "sit", "in,", "Lysandra", "Nov 18, 2020"],
+  [
+    9,
+    "dictum.",
+    "Cras pellentesque. Sed dictum. Proin eget odio.",
+    "Xantha",
+    "Apr 15, 2020",
+  ],
+  [10, "sem", "feugiat nec, diam.", "Kelsie", "Dec 14, 2019"],
+  [11, "gravida", "egestas. Aliquam", "Lacota", "Oct 26, 2020"],
+  [
+    12,
+    "dis",
+    "mauris a nunc. In at pede. Cras vulputate velit eu",
+    "Kyla",
+    "Jul 17, 2020",
+  ],
+  [13, "ridiculus", "lectus.", "Medge", "Feb 22, 2021"],
+  [
+    14,
+    "elementum",
+    "libero est, congue a, aliquet vel,",
+    "Justina",
+    "Apr 1, 2020",
+  ],
+  [15, "dolor", "felis eget varius ultrices,", "Hermione", "Apr 20, 2021"],
+];
+
+const posts = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default posts;
